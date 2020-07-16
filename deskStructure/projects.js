abee0c21728ef382b610bdadaaa726a448bb6b9a
warren.caruana@webriq.services <warrencaruana1@gmail.com>
2020-07-16 15:29:41 +0800

    Test

--- deskStructure/projects.js
+++
@@ -1,7 +0,0 @@
-import S from "@sanity/desk-tool/structure-builder";
-
-export default S.listItem()
-	.id("projects")
-	.schemaType("project")
-	.title("Projects")
-	.child(S.documentTypeList("project"));
